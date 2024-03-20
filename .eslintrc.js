3ea4d7814f4c5e0dbc0639da52414b5e3cf13c5f
najouss08 <l.najla1999@hotmail.fr>
2024-03-20 12:27:40 +0100

    loginPageAdded

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
+const { rules } = require("eslint-config-prettier");
+
 module.exports = {
   root: true,
   extends: '@react-native',
+
+rules:{
+  'prettier/prettier':0,
+},
 };
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,31 @@
+// GlobalStyles.js
+
+// Define your global styles
+export const Color = {
+    primary: '#007bff',
+    secondary: '#6c757d',
+    // Other color variables...
+  };
+  
+  export const Border = {
+    normal: '1px solid #ccc',
+    // Other border variables...
+  };
+  
+  export const FontSize = {
+    small: '12px',
+    medium: '16px',
+    // Other font size variables...
+  };
+  
+  export const FontFamily = {
+    default: 'Arial, sans-serif',
+    // Other font family variables...
+  };
+  
+  export const Padding = {
+    small: '5px',
+    medium: '10px',
+    // Other padding variables...
+  };
+  
